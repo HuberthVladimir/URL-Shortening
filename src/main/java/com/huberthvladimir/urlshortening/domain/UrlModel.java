86bcc99eb034d07cd4d7f742e7faa395237a3630
HuberthVladimir <vladimirhuberth@hotmail.com>
2024-10-14 18:25:26 -0300

    feat: add model URL

---
+++ src/main/java/com/huberthvladimir/urlshortening/domain/UrlModel.java
@@ -0,0 +1,75 @@
+package com.huberthvladimir.urlshortening.domain;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "URL_TABLE")
+@EntityListeners(AuditingEntityListener.class)
+public class UrlModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private String shortCode;
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate 
+    private Instant updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void SetUrl(String url) {
+        this.url = url;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updateAt) {
+        this.updatedAt = updateAt;
+    }
+}
