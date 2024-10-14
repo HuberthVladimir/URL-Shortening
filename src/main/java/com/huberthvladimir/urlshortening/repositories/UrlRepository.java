bfa3fb9540c9fa43b227c40921bbca4fc41f5079
HuberthVladimir <vladimirhuberth@hotmail.com>
2024-10-14 18:27:32 -0300

    feat: add url repository

---
+++ src/main/java/com/huberthvladimir/urlshortening/repositories/UrlRepository.java
@@ -0,0 +1,10 @@
+package com.huberthvladimir.urlshortening.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.huberthvladimir.urlshortening.domain.UrlModel;
+
+public interface UrlRepository extends JpaRepository<UrlModel, Integer>{
+    boolean existsByShortCode(String shortCode);
+    UrlModel findByUrlContaining(String url);
+}
