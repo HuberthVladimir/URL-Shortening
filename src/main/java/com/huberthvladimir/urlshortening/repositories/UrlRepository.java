abd13caca8d7f592e03e836b232f10fed6ae08dd
HuberthVladimir <vladimirhuberth@hotmail.com>
2024-10-24 19:58:12 -0300

    refactor: change name of interface methods

--- src/main/java/com/huberthvladimir/urlshortening/repositories/UrlRepository.java
+++ src/main/java/com/huberthvladimir/urlshortening/repositories/UrlRepository.java
@@ -6,6 +6,6 @@ import com.huberthvladimir.urlshortening.domain.UrlModel;
 
 public interface UrlRepository extends JpaRepository<UrlModel, Integer>{
     boolean existsByShortCode(String shortCode);
-    UrlModel findByShortCodeContaining(String shortCode);
-    UrlModel findByUrlContaining(String url);
+    UrlModel findByShortCode(String shortCode);
+    UrlModel findByUrl(String url);
 }
